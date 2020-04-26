b3fe7dca55f3bb7eab8e341583fcce22cbc42d32
Brev860 <60907093+Brev860@users.noreply.github.com>
2020-04-25 20:48:29 -0400

    Add files via upload

---
+++ creating_a_list/index.js
@@ -0,0 +1,27 @@
+var names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
+
+for (i = 0; i < 10; i++){
+    var greeting = document.createElement("h1");
+greeting.textContent = "Hello World";
+document.body.append(greeting);
+greeting.style.backgroundColor = "red";
+greeting.style.color = "white";
+}
+
+for (i = 0; i < 8; i++){
+    var greeting2 = document.createElement("h1");
+greeting2.textContent = "Hello World";
+document.body.append(greeting2);
+greeting2.style.backgroundColor = "grey";
+greeting2.style.color = "blue";
+}
+
+for (i = 0; i < names.length; i++){
+    var multGreeting = document.createElement("h1");
+     multGreeting.textContent = names[i];
+     document.body.append(multGreeting);
+     multGreeting.style.backgroundColor = "blue";
+     multGreeting.style.color = "red";
+     
+}
+
